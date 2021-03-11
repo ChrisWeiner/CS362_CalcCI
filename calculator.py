50d62a13e4f63a181487dc5f6078c5a86e7fb188
Christopher Weiner <chriscweiner@gmail.com>
2021-03-10 22:30:25 -0800

    Initial commit of testing files, as well as coverage report

---
+++ calculator.py
@@ -0,0 +1,8 @@
+#Christopher Weiner
+#CS 362 - Winter 2021
+
+def add(a,b):
+    return a + b
+
+def subtract(a,b):
+    return a - b
---
+++ test_calculator.py
@@ -0,0 +1,11 @@
+#Christopher Weiner
+#CS 362 - Winter 2021
+import calculator as calc
+
+class TestCalculator:
+
+    def test_add(self):
+        assert 6 == calc.add(1,5)
+
+    def test_subtract(self):
+        assert 2 == calc.subtract(4,2)
