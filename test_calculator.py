126df57df47a509455b109b578771e35a3f1b211
Christopher Weiner <chriscweiner@gmail.com>
2021-03-10 22:45:26 -0800

    Second update of .py files for circleCI

--- test_calculator.py
+++ test_calculator.py
@@ -4,7 +4,8 @@ import calculator as calc
 class TestCalculator:
 
     def test_add(self):
-        assert 6 == calc.add(1,5)
+        assert 6 == calc.add(1, 5)
 
     def test_subtract(self):
-        assert 2 == calc.subtract(4,2)
+        assert 2 == calc.subtract(4, 2)
+        
